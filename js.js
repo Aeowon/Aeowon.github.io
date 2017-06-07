c153bba68926c79f322b6c4b325c2dcdc28374d4
Matt Smith <Aeowon@users.noreply.github.com>
2017-06-06 23:35:58 -0700

    web

---
+++ js.js
@@ -0,0 +1,95 @@
+/*
+
+            ** UPDATE **
+
+- Added one PDF for Java and C++
+
+- Kotlin is now avaible with six resources!!!
+
+*/
+
+$(document).ready(function(){
+
+    $('#toggle-c').click(function(){
+        $('#desc-c').toggleClass('open');
+    });
+
+    $('#toggle-cpl').click(function(){
+        $('#desc-cpl').toggleClass('open');
+    });
+
+    $('#toggle-as').click(function(){
+        $('#desc-as').toggleClass('open');
+    });
+
+    $('#toggle-web').click(function(){
+        $('#desc-web').toggleClass('open');
+    });
+
+    $('#toggle-csharp').click(function(){
+        $('#desc-csharp').toggleClass('open');
+    });
+
+    $('#toggle-fsharp').click(function(){
+        $('#desc-fsharp').toggleClass('open');
+    });
+
+	
+/*
+    $('#toggle-java').click(function(){
+        $('#desc-java').toggleClass('open');
+    });
+    
+
+	$('#toggle-kotlin').click(function(){
+        $('#desc-kotlin').toggleClass('open');
+    });
+
+    $('#toggle-lua').click(function(){
+        $('#desc-lua').toggleClass('open');
+    });
+
+    $('#toggle-php').click(function(){
+        $('#desc-php').toggleClass('open');
+    });
+
+    $('#toggle-python').click(function(){
+        $('#desc-python').toggleClass('open');
+    });
+
+    $('#toggle-swift').click(function(){
+        $('#desc-swift').toggleClass('open');
+    });
+
+    $('#toggle-ruby').click(function(){
+        $('#desc-ruby').toggleClass('open');
+    });
+*/
+
+});
+
+// Information
+
+/******************************************
+*                                         *
+*    Made by: @Maz © 2017                 *
+*    Title: PDF Resources                 *
+*    Tecnologies: HTML, CSS, jQuery       *
+*                                         *
+*    Sources: sololearn.com               *
+*                                         *
+*    Extra Credits: @Filip,               *
+*    @Acke Strömberg, @Lana, @luka        *
+*                                         *
+******************************************/
+
+/*
+
+---------------------------------------------
+
+Some books are provided by Filip in this thread: 
+https://www.sololearn.com/Discuss/204501/literature-for-programming-languages-books
+
+---------------------------------------------
+
+*/
